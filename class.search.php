113f4c8e24b7e4b32aa55776ac5b686f7b1fa0fd
SeverVladMuresan <pc_freak_2005@yahoo.com>
2014-11-09 18:08:55 +0200

    Search by project title or project keywords possible.

--- class.search.php
+++ class.search.php
@@ -24,82 +24,147 @@ class Search {
     private $coordinators;
             
     //private $searchItem;
-    private $keywords;
+   
             
     function __construct($searchItem) {
         
         //$this->searchItem = $searchItem;
-        $this->keywords = explode( " " , $searchItem);
+        $this->searchItems = explode( " " , $searchItem);
+        
         $this->coordinators = User::listCoordinators();
+        
+        $this->prjects = Projects::listAllProjects();
     }
     
     function getSearchResults(){
         
         $searchResults = array();
         
-        foreach ($this->coordinators as $cKey => $coordinator) {
+        //REMOVE COMMENT FORM HERE
+//        
+//        
+//        
+//        
+//        foreach ($this->coordinators as $cKey => $coordinator) {
+//            
+//            //var_dump($cKey);
+//            $urlForCoordinators = "---";// TODO TEMPORARY --- it should be initialized in constants.php
+//            
+//            $fullName = $coordinator->firstName." ".$coordinator->lastName;
+//            $url = $urlForCoordinators+"/"+$coordinator->id;
+//            
+//            $firstNameAsArray = explode( " " , $coordinator->firstName);
+//            
+//            $searchResult = new SearchResult($fullName, $url);
+//            //var_dump($firstNameAsArray);
+//            $searchResult ->setNumberOfPossibleKeywordsHits((sizeof($firstNameAsArray) + 1)); // 1 means the lastName
+//            
+//            
+//            // Starting to search through coordinators
+//            
+//            foreach ($this->searchItems as $searchItem) {
+//                //strcasecmp($coordinator->lastName, $keyword) ;
+//                
+//                if(strcasecmp($coordinator->lastName, $searchItem) == 0 /*$coordinator->lastName === $keyword*/){     
+//                    //echo 'ENTERED!!!';
+//                    $searchResult->incrementKeyWordsHits();      
+//                }
+//                
+//                
+//                foreach ($firstNameAsArray as $fName) {
+//                    if(strcasecmp($fName, $searchItem) == 0/*$fName === $keyword*/){     
+//                        $searchResult->incrementKeyWordsHits();      
+//                    }              
+//                }
+//                
+//            } // end of checking every keyword
+//            
+//           
+//                    
+//            $searchResult->setKeywordsHitPercentage();
+//            
+//            if($searchResult->getKeywordsHitPercentage() > 0){
+//                
+//                array_push($searchResults, $searchResult);
+//                
+//            }
+//            
+//        }// end of itterating through coordinators
+//        
+//        
+//        
+//        
+        
+        
+        
+        //Starting to search through projects
+        //TODO
+        foreach ($this->prjects as $pKey => $project) {
             
-            var_dump($cKey);
+            var_dump($project);
+            $hits = array();
             
-            $urlForCoordinators = "---";// TEMPORARY --- it should be initialized in constants.php
             
-            $fullName = $coordinator->firstName." ".$coordinator->lastName;
-            $url = $urlForCoordinators+"/"+$coordinator->id;
+            $resultName = $project->title;
+            $urlForProjects = "--Project--";// TODO TEMPORARY --- it should be initialized in constants.php
+            $url = $urlForProjects+"/"+$project->id;
             
-            $firstNameAsArray = explode( " " , $coordinator->firstName);
             
-            $searchResult = new SearchResult($fullName, $url);
+            $searchResult = new SearchResult($resultName, $url);
+            
+            $projectNameAsKeyWords = explode(" " , $project->title);
+            
+            $numberOfPossibleKeywordsHits = sizeof($project->keyword) + sizeof($projectNameAsKeyWords);
+            
+            
             //var_dump($firstNameAsArray);
-            $searchResult ->setNumberOfPossibleKeywordsHits((sizeof($firstNameAsArray) + 1)); // 1 means the lastName
+            $searchResult ->setNumberOfPossibleKeywordsHits($numberOfPossibleKeywordsHits); 
             
             
-            // Starting to search through coordinators
-            
-            foreach ($this->keywords as $keyword) {
-                //strcasecmp($coordinator->lastName, $keyword) ;
+            foreach ($this->searchItems as $searchItem) {
                 
-                if(strcasecmp($coordinator->lastName, $keyword) == 0 /*$coordinator->lastName === $keyword*/){     
-                    //echo 'ENTERED!!!';
-                    $searchResult->incrementKeyWordsHits();      
-                }
-                
-                
-                foreach ($firstNameAsArray as $fName) {
-                    if(strcasecmp($fName, $keyword) == 0/*$fName === $keyword*/){     
-                        $searchResult->incrementKeyWordsHits();      
-                    }              
-                }
-                
-            } // end of checking every keyword
-            
-           
+                foreach ($projectNameAsKeyWords as $value) {
                     
-            $searchResult->setKeywordsHitPercentage();
+                    if(strcasecmp($value, $searchItem) == 0){     
+                        //TODO maybe make that particular 
+                        //var_dump($searchItem."---".$value);
+                        $searchResult->incrementKeyWordsHits();     
+                        array_push($hits, $value);
+                    }
+                    
+                }
+                
+                foreach ($project->keyword as $value) {
+                    
+                    if(strcasecmp($value, $searchItem) == 0){     
+                        //TODO maybe make that particular 
+                        //var_dump($searchItem."---".$value);
+                        $searchResult->incrementKeyWordsHits(); 
+                        array_push($hits, $value);
+                    }
+                    
+                }    
+                    
+                
+                
+                    
+            }//end of itterating through search items
             
+            
+            $searchResult->setKeywordsHitPercentage();
+      
             if($searchResult->getKeywordsHitPercentage() > 0){
-                
+              
                 array_push($searchResults, $searchResult);
-                
+              
             }
             
-        }// end of itterating through coordinators
-        
-        
-        
-        
-        
-        
-        
-        // Starting to search through projects
-        //NOT IMPLEMENTED YET
-        //TODO
-        
-        foreach ($this->prjects as $pKey => $value) {
             
             
-            
-        }
+        } // end of itteating through projects
         
+        
+        //All search results added at this point
         
         
         function compareSearchResult($a, $b)
@@ -107,7 +172,6 @@ class Search {
             //This is so that it will sort DESCENDING
             $aux = $b->getKeywordsHitPercentage() - $a->getKeywordsHitPercentage();
             //var_dump($aux);
-            
             if ($aux < 0) {
                 $aux = -1;
             } else if ($aux > 0) {
@@ -117,13 +181,11 @@ class Search {
             }
 
             return ($aux);  //strcmp($a->name, $b->name);
-            
-            
-        }           
 
+        }           
         //var_dump($searchResults);
-        usort($searchResults, "compareSearchResult");
         
+        usort($searchResults, "compareSearchResult");
         return  $searchResults;
         
     }
